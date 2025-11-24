512bb2b19baacf0688d5327b86af8e5567543b19
Арысбай Айбар Ғалиұлы <ARYSBAYA@bank.corp.centercredit.kz>
2025-11-24 08:59:19 +0500

    Go

---
+++ ConGitFirst/CEO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConGitFirst
+{
+    public class CEO
+    {
+        public void Earth()
+        {
+            Console.WriteLine("Niggaz");
+        }
+    }
+}
